bceaa8ea9241c2e633a52836f155946964ef0b8a
wangke168 <wangke168@gmail.com>
2016-09-01 16:37:50 +0800

    third commit

--- app/WeChat/Response.php
+++ app/WeChat/Response.php
@@ -14,7 +14,7 @@ use EasyWeChat\Foundation\Application;
 use DB;
 use EasyWeChat\Message\News;
 use EasyWeChat\Message\Text;
-use App\WeChat\usage;
+use App\WeChat\Usage;
 //use App\Http\Requests;
 use Crypt;
 
