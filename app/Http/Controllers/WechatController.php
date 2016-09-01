809ca28102283a488425b50b494dedd4b4f7e1a8
wangke168 <wangke168@gmail.com>
2016-09-01 15:51:41 +0800

    third commit

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -22,8 +22,10 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
+        $userService = $wechat->user;
+        $wechat->server->setMessageHandler(function($message) use ($userService){
+            $openid = $userService->get($message->FromUserName)->openid;
+            return "欢迎关注 overtrue！".$openid;
         });
 
         Log::info('return response.');
