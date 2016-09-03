2c0670dde9877d0d3b5ba119856baedc95858b7d
wangke168 <wangke168@gmail.com>
2016-09-03 10:36:45 +0800

    fix content commit

--- app/WeChat.php
+++ app/WeChat.php
@@ -10,6 +10,8 @@ class WeChat extends Model
     protected $table= 'wx_article';
     public function scopePublish($query)
     {
-        return $query->where('online', '1');
+        return $query->where('audit', '1')
+            ->where('del', '0')
+            ->where('online', '1');
     }
 }
