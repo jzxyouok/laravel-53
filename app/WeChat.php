627340e029d52817397eee0deb03d199b14343fe
wangke168 <wangke168@gmail.com>
2016-09-03 10:35:45 +0800

    fix content commit

--- app/WeChat.php
+++ app/WeChat.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class WeChat extends Model
 {
     //
-    protected $table= 'wx_articel';
+    protected $table= 'wx_article';
     public function scopePublish($query)
     {
         return $query->where('online', '1');
